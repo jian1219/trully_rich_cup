c94aaeaa1cca1f3e5173ab4cdc490828c8eade95
jian1219 <jian70227@gmail.com>
2025-09-25 21:21:33 +0800

    Add new files

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
+import { useNavigate } from 'react-router-dom';
+
 function Homepage() {
+
+    const navigate = useNavigate();
+
+    const goToAnotherPage = () => {
+      navigate('/Trully-Rich-Cup-adminHome');
+    };
   return (
     <div>
         <h1>Home Marie gwapa</h1>
 
-        <a href="/Trully-Rich-Cup-adminHome">asas</a>
+        <div className='header-right'>
+           
+            <button onClick={goToAnotherPage}>admin</button>
+        </div>
     </div>
   )
 }
